f433a924095621110d6c0cdb7d22138eff61ffe7
yashdeshmukh980 <yashdeshmukh980@gmail.com>
2025-10-14 23:13:15 +0530

    circle

---
+++ circle.java
@@ -0,0 +1,12 @@
+class demo
+{
+    public static void main (String [] args)
+    {
+        int r=5;
+        double a,c;
+        a=3.14*r*r;
+        c=2*3.14*r;
+        System.out.println("Area is "+a);
+        System.err.println("Circumference is "+c);
+    }    
+}
