5cc659d913db81f71cc1e9ea0f8498e155a8a76e
yashdeshmukh980 <yashdeshmukh980@gmail.com>
2025-10-14 20:58:43 +0530

    marks total

---
+++ marks.java
@@ -0,0 +1,9 @@
+class demo
+{
+    public static void main (String [] args)
+    {
+        int a = 50,b=34,c=56,d=58;
+        int totalMarks=a+b+c+d;
+        System.out.print("Total Marks is "+totalMarks);
+    }
+}
